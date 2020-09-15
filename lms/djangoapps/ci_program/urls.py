d732cb4e5467864686f8a02bd4840f3efd182c4f
Ray <gomyar@users.noreply.github.com>
2020-09-15 09:45:27 +0100

    Adding program bookmarks view (#16)

--- lms/djangoapps/ci_program/urls.py
+++ lms/djangoapps/ci_program/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from .views import show_programs
+from .views import show_programs, show_program_bookmarks
 
 urlpatterns = [
-    url(r'^(?P<program_name>[\w\-]+)', show_programs, name='show_programs'),
+    url(r'^(?P<program_name>[\w\-]+)$', show_programs, name='show_programs'),
+    url(r'^(?P<program_name>[\w\-]+)/bookmarks', show_program_bookmarks, name='show_program_bookmarks'),
 ]
--- lms/djangoapps/ci_program/views.py
+++ lms/djangoapps/ci_program/views.py
@@ -1,6 +1,10 @@
+
+import math
+
 from django.contrib.auth.decorators import login_required
 from edxmako.shortcuts import render_to_response
 from ci_program.models import Program
+from openedx.core.djangoapps.bookmarks.models import Bookmark
 
 
 @login_required
@@ -38,3 +42,26 @@ def show_programs(request, program_name):
         'program_slug': program_name,
     }
     return render_to_response('programs/programs.html', context)
+
+
+@login_required
+def show_program_bookmarks(request, program_name):
+    """
+    Display the programs page
+    """
+    page = int(request.GET.get('page', 1))
+    page_size = int(request.GET.get('page_size', 10))
+
+    program = Program.objects.get(marketing_slug=program_name)
+    bookmarks = Bookmark.objects.filter(
+        course_key__in=program.get_course_locators(),
+        user=request.user)
+    page_data = bookmarks[(page - 1) * page_size: page * page_size]
+
+    return render_to_response('programs/program_bookmarks.html', {
+        "bookmarks": page_data,
+        "page": page,
+        "page_size": page_size,
+        "total_pages": math.ceil(len(bookmarks) / page_size),
+        "program_name": program_name,
+    })
