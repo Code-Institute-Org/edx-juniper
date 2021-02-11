624f446e144cd847c9e6fe6d0a53a0bbfbfbed0c
Nakita <nakitamccool@gmail.com>
2021-02-11 13:05:41 +0000

    Refactor
    Add new fields for each key step in the command - connect to the db, get collection, get students, get challenges, export results to HubSpot or Zoho

--- lms/djangoapps/learning_success/management/commands/export_coding_challenge_data.py
+++ lms/djangoapps/learning_success/management/commands/export_coding_challenge_data.py
@@ -43,182 +43,6 @@ CODING_CHALLENGES = [
     "lesson_5_challenge_3"
 ]
 
-
-def get_students(program_code):
-    """Get students enrolled in a particular program
-    Return dict in following format {id: email}
-    """
-    program = get_program_by_program_code(program_code)
-    enrolled_students = program.enrolled_students.all()
-    return {student.id: student.email for student in enrolled_students}
-
-
-def get_challenges(db):
-    """Return dict of challenges in following format {id: name}"""
-    collection = db["challenges"]
-    challenges_query = collection.find({"name": {"$in": CODING_CHALLENGES}})
-    challenges = {challenge.get("_id"): challenge.get("name") for challenge in challenges_query}
-    return challenges
-
-
-def get_submissions(db, challenges, students, submitted_since):
-    """Get submissions for challenges submitted by enrolled students
-    within a particular time frame e.g. submitted_since_yesterday
-    """
-    submissions_since_yday = db.submissions.find(
-        {
-            "submitted": {"$gte": submitted_since},
-            "challenge_id": {"$in": list(challenges.keys())},
-            "user_id": {"$in": list(students.keys())}
-        }
-    ).sort("submitted", pymongo.DESCENDING)
-    return submissions_since_yday
-
-
-def get_results_for_all_students(program_code, dbname):
-    """Get results from challenge submissions for all students
-    enrolled in a given program.
-
-    The sequence of events are as follows:
-    1. Get all students enrolled in program
-    2. Get ids for all challenges in coding challenge program
-    3. Get all submissions for coding challenges,
-       submitted by an enrolled student in the last 25 hours.
-       Allowing an additional hour to account for any submissions that occur while script is running.
-    4. Submissions are returned sorted by submission date, latest submission first.
-       We only require the result of the latest submission of a particular challenge
-       i.e. first submission found in results (which is sorted by latest submission first)
-       Previous submissions are skipped
-    5. Format for HubSpot
-       results = [
-           "student@email.com" : {
-               "lesson_1_challenge_1" : "Pass"
-           }
-       ]
-    """
-    students = get_students(program_code)
-    challenges_db = settings.MONGO_CLIENT[dbname]
-    challenges = get_challenges(challenges_db)
-
-    one_day_ago = datetime.today() - timedelta(days=1)
-    submissions_since_yday = get_submissions(challenges_db, challenges, students, one_day_ago)
-
-    results = {}
-    for submission in submissions_since_yday:
-        email = students[submission.get("user_id")]
-        if email not in results:
-            results.setdefault(email, {})
-
-        challenge = challenges[submission.get("challenge_id")]
-        result = 'Pass' if submission.get("passed") else 'Fail'
-
-        if challenge not in results[email]:
-            results[email][challenge] = result
-
-    return results
-
-
-def post_to_hubspot(endpoint, student, properties):
-    """Post results of challenge submissions to the student profiles on HubSpot"""
-    url = "%s/email/%s/profile?hapikey=%s" % (
-        endpoint, student, HUBSPOT_API_KEY)
-    headers = {
-        "Content-Type": "application/json"
-    }
-    data = json.dumps({
-        "properties": properties
-    })
-    response = requests.post(
-        data=data, url=url, headers=headers)
-    if response.status_code == 204:
-        log.info("Challenge results recorded for: %s" % (student))
-    else:
-        log.info(
-            "Attempt to send challenge results for %s to HubSpot " \
-            "failed with following response %s: %s" % (
-                student, response.status_code, response.json))
-
-
-def get_access_token():
-    """Retrieve a Zoho CRM access token.
-    This may fail due to various network/throttling issues, so we will
-    retry
-    """
-    for attempt in range(REFRESH_RETRIES):
-        try:
-            refresh_resp = requests.post(REFRESH_ENDPOINT, params={
-                "refresh_token": REFRESH_TOKEN,
-                "client_id": CLIENT_ID,
-                "client_secret": CLIENT_SECRET,
-                "grant_type": "refresh_token"
-            })
-            log.info("Successfully retrieved Zoho token")
-            return refresh_resp.json()['access_token']
-        except KeyError as e:
-            log.info(
-                "ERROR: Getting Zoho token attempt %s out of %s " \
-                "failed with the exception: %s" % (attempt, REFRESH_RETRIES, e)
-            )
-            time.sleep(REFRESH_SLEEP_SECS)
-
-
-def get_auth_headers():
-    """Return HTTP headers to authenticate against Zoho CRM."""
-    access_token = get_access_token()
-    return {"Authorization": "Zoho-oauthtoken " + access_token}
-
-
-def post_to_learningpeople(CHALLENGE_ENDPOINT, auth_headers, json, student):
-    response = requests.post(
-        CHALLENGE_ENDPOINT,
-        headers=auth_headers,
-        json=json
-    )
-    if response.status_code == 200:
-        log.info("Challenge results recorded for: %s" % (student))
-    else:
-        log.info(
-            "Attempt to send challenge results for %s to LP " \
-            "failed with the following response %s: %s" % (
-                student, response.status_code, response.json))  
-
-
-def export_challenges_submitted(program_code, dbname):
-    """Get results for all students and prepare those results in a format
-    that can be posted to Zoho if student is on an Learning People program,
-    else HubSpot profiles for all other students
-    """
-    results_for_all_students = get_results_for_all_students(program_code, dbname)
-    if program_code == "LPCC":
-        auth_headers_for_zoho = get_auth_headers()
-        for student, results in results_for_all_students.items():
-            json_for_zoho = {
-                "data": [{"Email": student}],
-                "duplicate_check_fields": ["Email"],
-            }
-            for challenge_name, result in results.items():
-                json_for_zoho["data"][0][challenge_name] = result
-
-            post_to_learningpeople(
-                CHALLENGE_ENDPOINT,
-                auth_headers_for_zoho,
-                json_for_zoho,
-                student
-            )
-    else:
-        for student, results in results_for_all_students.items():
-            properties = [{
-                "property": "email",
-                "value": student
-            }]
-            for challenge_name, result in results.items():
-                properties.append({
-                    "property": challenge_name,
-                    "value": result
-                })
-            post_to_hubspot(HUBSPOT_CONTACTS_ENDPOINT, student, properties)
-
-
 class Command(BaseCommand):
     help = 'Post the results of challenge submissions submitted in the last day for a given coding challenge program'
 
@@ -227,4 +51,181 @@ class Command(BaseCommand):
         parser.add_argument('--dbname', type=str, default='challenges')
 
     def handle(self, program_code, **kwargs):
-        export_challenges_submitted(program_code, kwargs['dbname'])
+        self.db = settings.MONGO_CLIENT[kwargs['dbname']]
+        self.collection = self.db["challenges"]
+        self.program_code = program_code
+        self.students = self.get_students()
+        self.challenges = self.get_challenges()
+
+        self.export_challenges_submitted()
+
+    def get_students(self):
+        """Get students enrolled in a particular program
+        Return dict in following format {id: email}
+        """
+        program = get_program_by_program_code(self.program_code)
+        enrolled_students = program.enrolled_students.all()
+        return {student.id: student.email for student in enrolled_students}
+
+    def get_challenges(self):
+        """Return dict of challenges in following format {id: name}"""
+        challenges_query = self.collection.find({"name": {"$in": CODING_CHALLENGES}})
+        challenges = {challenge.get("_id"): challenge.get("name") for challenge in challenges_query}
+        return challenges
+
+    def get_submissions(self, challenges, students, submitted_since):
+        """Get submissions for challenges submitted by enrolled students
+        within a particular time frame e.g. submitted_since_yesterday
+        """
+        submissions_since_yday = self.db.submissions.find(
+            {
+                "submitted": {"$gte": submitted_since},
+                "challenge_id": {"$in": list(challenges.keys())},
+                "user_id": {"$in": list(students.keys())}
+            }
+        ).sort("submitted", pymongo.DESCENDING)
+        return submissions_since_yday
+
+
+    def get_results_for_all_students(self):
+        """Get results from challenge submissions for all students
+        enrolled in a given program.
+
+        The sequence of events are as follows:
+        1. Get all students enrolled in program
+        2. Get ids for all challenges in coding challenge program
+        3. Get all submissions for coding challenges,
+        submitted by an enrolled student in the last 25 hours.
+        Allowing an additional hour to account for any submissions that occur while script is running.
+        4. Submissions are returned sorted by submission date, latest submission first.
+        We only require the result of the latest submission of a particular challenge
+        i.e. first submission found in results (which is sorted by latest submission first)
+        Previous submissions are skipped
+        5. Format for HubSpot
+        results = [
+            "student@email.com" : {
+                "lesson_1_challenge_1" : "Pass"
+            }
+        ]
+        """
+
+        one_day_ago = datetime.today() - timedelta(days=1)
+        submissions_since_yday = self.get_submissions(
+            self.challenges, self.students, one_day_ago)
+
+        results = {}
+        for submission in submissions_since_yday:
+            email = self.students[submission.get("user_id")]
+            if email not in results:
+                results.setdefault(email, {})
+
+            challenge = self.challenges[submission.get("challenge_id")]
+            result = 'Pass' if submission.get("passed") else 'Fail'
+
+            if challenge not in results[email]:
+                results[email][challenge] = result
+
+        return results
+
+
+    def post_to_hubspot(self, endpoint, student, properties):
+        """Post results of challenge submissions to the student profiles on HubSpot"""
+        url = "%s/email/%s/profile?hapikey=%s" % (
+            endpoint, student, HUBSPOT_API_KEY)
+        headers = {
+            "Content-Type": "application/json"
+        }
+        data = json.dumps({
+            "properties": properties
+        })
+        response = requests.post(
+            data=data, url=url, headers=headers)
+        if response.status_code == 204:
+            log.info("Challenge results recorded for: %s" % (student))
+        else:
+            log.info(
+                "Attempt to send challenge results for %s to HubSpot " \
+                "failed with following response %s: %s" % (
+                    student, response.status_code, response.json))
+
+
+    def get_access_token(self):
+        """Retrieve a Zoho CRM access token.
+        This may fail due to various network/throttling issues, so we will
+        retry
+        """
+        for attempt in range(REFRESH_RETRIES):
+            try:
+                refresh_resp = requests.post(REFRESH_ENDPOINT, params={
+                    "refresh_token": REFRESH_TOKEN,
+                    "client_id": CLIENT_ID,
+                    "client_secret": CLIENT_SECRET,
+                    "grant_type": "refresh_token"
+                })
+                log.info("Successfully retrieved Zoho token")
+                return refresh_resp.json()['access_token']
+            except KeyError as e:
+                log.info(
+                    "ERROR: Getting Zoho token attempt %s out of %s " \
+                    "failed with the exception: %s" % (attempt, REFRESH_RETRIES, e)
+                )
+                time.sleep(REFRESH_SLEEP_SECS)
+
+
+    def get_auth_headers(self):
+        """Return HTTP headers to authenticate against Zoho CRM."""
+        access_token = self.get_access_token()
+        return {"Authorization": "Zoho-oauthtoken " + access_token}
+
+
+    def post_to_learningpeople(self, CHALLENGE_ENDPOINT, auth_headers, json, student):
+        response = requests.post(
+            CHALLENGE_ENDPOINT,
+            headers=auth_headers,
+            json=json
+        )
+        if response.status_code == 200:
+            log.info("Challenge results recorded for: %s" % (student))
+        else:
+            log.info(
+                "Attempt to send challenge results for %s to LP " \
+                "failed with the following response %s: %s" % (
+                    student, response.status_code, response.json))  
+
+
+    def export_challenges_submitted(self):
+        """Get results for all students and prepare those results in a format
+        that can be posted to Zoho if student is on an Learning People program,
+        else HubSpot profiles for all other students
+        """
+        results_for_all_students = self.get_results_for_all_students()
+        if self.program_code == "LPCC":
+            auth_headers_for_zoho = self.get_auth_headers()
+            for student, results in results_for_all_students.items():
+                json_for_zoho = {
+                    "data": [{"Email": student}],
+                    "duplicate_check_fields": ["Email"],
+                }
+                for challenge_name, result in results.items():
+                    json_for_zoho["data"][0][challenge_name] = result
+
+                self.post_to_learningpeople(
+                    CHALLENGE_ENDPOINT,
+                    auth_headers_for_zoho,
+                    json_for_zoho,
+                    student
+                )
+        else:
+            for student, results in results_for_all_students.items():
+                properties = [{
+                    "property": "email",
+                    "value": student
+                }]
+                for challenge_name, result in results.items():
+                    properties.append({
+                        "property": challenge_name,
+                        "value": result
+                    })
+                self.post_to_hubspot(HUBSPOT_CONTACTS_ENDPOINT, student, properties)
+
+
