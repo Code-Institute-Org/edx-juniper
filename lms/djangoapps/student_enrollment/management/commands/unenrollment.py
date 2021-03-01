f07bbcec3f62d112f82abc2ca68398dbbfca3581
Nakita <nakitamccool@gmail.com>
2021-03-01 15:57:10 +0000

    Add comments

--- lms/djangoapps/student_enrollment/management/commands/unenrollment.py
+++ lms/djangoapps/student_enrollment/management/commands/unenrollment.py
@@ -24,6 +24,19 @@ class Command(BaseCommand):
         This will retrieve all of the users from the Zoho CRM API and
         will unenroll all of the students that have a status of
         `Unenroll`.
+
+        Steps are as follows:
+        1. Get all students to be unenrolled from the CRM
+        2. For each student, check the following:
+            - a registered user account
+            - the programme id in their CRM profile maps to a valid program on the LMS
+            - they are enrolled in said program
+        3. If student passes all the above checks:
+            - Deactivate course enrollments (by setting is active to False) for all
+            courses in the given program
+            - Update the CRM profile by setting the LMS Access field to removed
+        4. If the student does not pass all the checks in point 2, trigger a zap which
+        emails the SC team with a description of the issue encountered
         """
         self.students = get_students_to_be_unenrolled()
         self.unenroll()
