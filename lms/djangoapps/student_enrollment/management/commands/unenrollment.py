9319b4d6733fce8bbf97f3a1c0d9a85aa07a46e1
Nakita <nakitamccool@gmail.com>
2021-03-04 11:47:26 +0000

    Fix import statement, introduced bug when refactoring

--- lms/djangoapps/student_enrollment/management/commands/unenrollment.py
+++ lms/djangoapps/student_enrollment/management/commands/unenrollment.py
@@ -5,10 +5,10 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.models import User
 from django.conf import settings
 from ci_program.models import Program
+from student_enrollment.utils import post_to_zapier
 from student_enrollment.zoho import (
     get_students_to_be_unenrolled,
-    update_student_record,
-    post_to_zapier
+    update_student_record
 )
 
 log = getLogger(__name__)
