a73f861c32de56ba6bf353d9eaf0e5a54ba77e40
Stefan Dworschak <stef.dworschak@gmail.com>
2021-03-10 11:35:01 +0000

    Fixing spaces

--- lms/djangoapps/learning_success/management/commands/create_lms_records.sql
+++ lms/djangoapps/learning_success/management/commands/create_lms_records.sql
@@ -1,10 +1,10 @@
 CREATE TABLE lms_records (
     `id` INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	`email` TEXT NOT NULL,
-	`student_data` TEXT,
+    `email` TEXT NOT NULL,
+    `student_data` TEXT,
     `pathway` text DEFAULT NULL,
     `source_platform` text DEFAULT NULL,
-	`state` VARCHAR(255) NOT NULL,
-	CHECK (`student_data` IS NULL OR JSON_VALID(`student_data`))
+    `state` VARCHAR(255) NOT NULL,
+    CHECK (`student_data` IS NULL OR JSON_VALID(`student_data`))
 );
