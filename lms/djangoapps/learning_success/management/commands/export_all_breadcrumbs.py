3d4e85952326132d31d344befa8315a58a97480e
ray <ray@codeinstitute.net>
2021-07-20 10:38:39 +0100

    Fixing typeo

--- lms/djangoapps/learning_success/management/commands/export_all_breadcrumbs.py
+++ lms/djangoapps/learning_success/management/commands/export_all_breadcrumbs.py
@@ -12,7 +12,7 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('programme_id', type=str)
 
-    def handle(self, programme_id):
+    def handle(self, programme_id, **kwargs):
         log.info("Running task export_all_breadcrumbs...")
 
         result = export_all_breadcrumbs.apply_async(args=[programme_id])
