fe0b759c677c06b917d71a6e6a1cf0ca578349b5
Igor <igor.basuga@gmail.com>
2022-03-09 14:58:49 +0100

    add SpecialisationEnrollment class and corresponding enroll method

--- lms/djangoapps/student_enrollment/enrollment.py
+++ lms/djangoapps/student_enrollment/enrollment.py
@@ -10,7 +10,8 @@ from student_enrollment.utils import (
     post_to_zapier
 )
 from student_enrollment.zoho import (
-    get_students_to_be_enrolled
+    get_students_to_be_enrolled,
+    get_students_to_be_enrolled_into_specialisation
 )
 from student_enrollment.models import EnrollmentStatusHistory
 from student_enrollment.models import ProgramAccessStatus
@@ -124,6 +125,109 @@ class Enrollment:
                 registered=bool(user),
                 enrollment_type=enrollment_type,
                 enrolled=bool(program_enrollment_status),
+                email_sent=email_sent_status
+            )
+            enrollment_status.save()
+
+
+class SpecialisationEnrollment:
+    ''' 
+    Enroll students in their relevant specialisation; simultaneously
+    unenroll them from the Common Curriculum programme
+    '''
+    def __init__(self, dryrun=False):
+        self.dryrun = dryrun
+
+    def enroll(self):
+        """
+        The main handler for the specialisation enrollment management command.
+        This will retrieve all of the users from the Zoho CRM API and
+        will enroll all of the students that have a specialisation enrollment 
+        status of `Approved`.
+        """
+        zoho_students = get_students_to_be_enrolled_into_specialisation()
+
+        for student in zoho_students:
+            if not student['Email']:
+                continue
+            if self.dryrun:
+                log.info("** dryrun attempting enrollment of student: %s",
+                         student['Email'])
+                continue
+
+            # Get the user, the user's password, and their enrollment type
+            user, password, enrollment_type = get_or_register_student(
+                student['Email'], student['Email'])
+
+            # Get the code of the specialisation to be enrolled in, as well
+            # as the current programme to be susequently unenrolled from
+            current_program = student["Programme_ID"]
+            specialisation_to_enroll = student['Specialisation_programme_id']
+
+            try:
+                # Get the Program that contains the Zoho specialisation program code
+                specialisation = Program.objects.get(
+                    program_code=specialisation_to_enroll)
+            except ObjectDoesNotExist as does_not_exist_exception:
+                log.exception("Could not find specialisation: %s")
+                post_to_zapier(
+                    settings.ZAPIER_ENROLLMENT_EXCEPTION_URL,
+                    {
+                        'email': student['Email'],
+                        'crm_field': 'Specialisation_programme_id',
+                        'unexpected_value': student['Specialisation_programme_id'],
+                        'attempted_action': 'enroll specialisation',
+                        'message': 'Specialisation programme ID does not exist on LMS'
+                    }
+                )
+                continue
+
+            # Enroll the student in the specialisation
+            specialisation_enrollment_status = specialisation.enroll_student_in_program(
+                user.email,
+                exclude_courses=EXCLUDED_FROM_ONBOARDING
+            )
+
+            # if specialisation enrollment successful, unenroll the 
+            # student from the previous (CC) programme
+            if specialisation_enrollment_status:
+                program_to_unenroll = Program.objects.get(
+                    program_code=specialisation_to_enroll
+                )
+                program_to_unenroll.unenroll_student_from_program(user.email)
+
+                # send the enrollment email
+                email_sent_status = specialisation.send_email(
+                    user, enrollment_type, password
+                )
+
+            # TODO: check if this is necessary for specialisations
+            # # Set the students access level (i.e. determine whether or
+            # # not a student is allowed to access to the LMS.
+            # # Deprecated...
+            # access, created = ProgramAccessStatus.objects.get_or_create(
+            #     user=user, program_access=True
+            # )
+
+            # if not created:
+            #     access.allowed_access = True
+            #     access.save()
+
+            # TODO: check if the existing Zap can be adjusted
+            # TODO: or a different Zap needs to be created
+            # # Used to update the status from 'Enroll' to 'Online'
+            # # in the CRM
+            # post_to_zapier(
+            #     settings.ZAPIER_ENROLLMENT_URL,
+            #     {'email': user.email}
+            # )
+
+            enrollment_status = EnrollmentStatusHistory(
+                student=user,
+                program=specialisation,
+                registered=bool(user),
+                enrollment_type=enrollment_type,
+                enrolled=bool(specialisation_enrollment_status),
                 email_sent=email_sent_status
             )
             enrollment_status.save()
