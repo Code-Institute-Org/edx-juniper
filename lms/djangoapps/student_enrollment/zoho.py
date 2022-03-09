480984634f772876a9257689ee1ba68e9715f42d
Igor <igor.basuga@gmail.com>
2022-03-09 14:25:15 +0100

    add Specialisation enrollment COQL query and call method

--- lms/djangoapps/student_enrollment/zoho.py
+++ lms/djangoapps/student_enrollment/zoho.py
@@ -37,6 +37,20 @@ WHERE ((
 )
 LIMIT {page},{per_page}
 """
+
+ENROLL_SPECIALISATION_QUERY = """
+SELECT Email, Full_Name, Programme_ID, Specialisation_programme_id
+FROM Contacts
+WHERE ((
+        (Specialisation_Enrollment_Status = 'Approved') AND (Specialisation_programme_id is not null)
+    )
+    AND (
+        (LMS_Version = 'Upgrade to Juniper') OR (LMS_Version = 'Juniper (learn.codeinstitute.net)')
+    )
+)
+LIMIT {page},{per_page}
+"""
+
 ENROLL_IN_CAREERS_MODULE_QUERY = """
 SELECT Email, Full_Name, Programme_ID
 FROM Contacts
@@ -74,6 +88,30 @@ def get_students_to_be_enrolled():
         if not students_resp.json()['info']['more_records']:
             return students
 
+
+def get_students_to_be_enrolled_into_specialisation():
+    """Fetch from Zoho all students
+    with a Specialisation Enrollment Status of 'Approved'
+    API documentation for this endpoint:
+    https://www.zohoapis.com/crm/v2/coql
+    """
+    students = []
+    auth_headers = get_auth_headers()
+
+    for page in count():
+        query = ENROLL_SPECIALISATION_QUERY.format(
+                    page=page*RECORDS_PER_PAGE,
+                    per_page=RECORDS_PER_PAGE)
+        students_resp = requests.post(
+            COQL_ENDPOINT,
+            headers=auth_headers,
+            json={"select_query":query})
+        if students_resp.status_code != 200:
+            return students
+
+        students.extend(students_resp.json()['data'])
+        if not students_resp.json()['info']['more_records']:
+            return students
 
 def get_students_to_be_unenrolled():
     """Fetch from Zoho all students
