cf7d1a917f79a251b7a59bd4e8fca2caaa1a51d2
wondrousWebWorks <alberts.johann@gmail.com>
2022-07-01 09:35:51 +0100

    Add env variable for dataproduct

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -3962,3 +3962,4 @@ AMOS_DB = os.environ.get('AMOS_DB')
 LRS_MONGO_HOST = os.environ.get('LRS_MONGO_HOST', 'mongodb://localhost:27017')
 LRS_MONGO_CLIENT = MongoClient(host=LRS_MONGO_HOST, readPreference='secondaryPreferred')
 LRS_MONGO_DB = LRS_MONGO_CLIENT[os.environ.get('LRS_MONGO_DB', 'dataproduct')]
+LMS_STUDENT_PROJECT_AND_PROGRESS_DATA = os.environ.get('LMS_STUDENT_PROJECT_AND_PROGRESS_DATA', 'lms_student_project_and_progress_data')
