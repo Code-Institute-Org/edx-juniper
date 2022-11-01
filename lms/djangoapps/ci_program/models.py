b615923cb28bb0891b51143865cee400b0be09a7
Igor <igor.basuga@gmail.com>
2022-11-01 16:20:25 +0100

    Improve code readability

--- lms/djangoapps/ci_program/models.py
+++ lms/djangoapps/ci_program/models.py
@@ -24,6 +24,7 @@ class Program(TimeStampedModel):
     """
     Representation of a Program.
     """
+
     STATUS_CHOICES = [
         ('live', 'Live'),
         ('in_development', 'In Development'),
