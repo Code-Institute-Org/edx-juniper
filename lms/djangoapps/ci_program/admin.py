029ebfa1afa3519a132732eb441540a8c0d7a744
Igor <igor.basuga@gmail.com>
2023-01-16 10:47:57 +0100

    Override default Program admin form to filter current instance from ManyToMany lists

--- lms/djangoapps/ci_program/admin.py
+++ lms/djangoapps/ci_program/admin.py
@@ -1,8 +1,18 @@
+from django import forms
 from django.contrib import admin
 from ci_program.models import ProgramCourseCode, CourseCode, Program
 
 
+class ProgramAdminForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ProgramAdminForm, self).__init__(*args, **kwargs)
+        self.fields['sample_content'].queryset = Program.objects.exclude(id=self.instance.id)
+        self.fields['support_programs'].queryset = Program.objects.exclude(id=self.instance.id)
+
+
 class ProgramAdmin(admin.ModelAdmin):
+    form = ProgramAdminForm
+
     list_display = (
         "program_code",
         "status",
