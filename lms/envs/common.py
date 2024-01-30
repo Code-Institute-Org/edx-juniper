c7a7cc2645daa43aa1a07e9f204ca4e2c52618ea
wondrousWebWorks <alberts.johann@gmail.com>
2024-01-30 16:10:47 +0000

    Modify order of default rest authentication classes

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -2598,9 +2598,9 @@ REST_FRAMEWORK = {
         'registration_validation': '30/minute',
     },
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'edx_rest_framework_extensions.auth.jwt.authentication.JwtAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'edx_rest_framework_extensions.auth.jwt.authentication.JwtAuthentication'
     ),
 }
 
