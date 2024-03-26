ded76ea31e13ac83ad504f5242fbe9fd34b60c23
wondrousWebWorks <alberts.johann@gmail.com>
2024-03-26 10:45:17 +0000

    Change bracket type for authentication settings

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -2597,11 +2597,11 @@ REST_FRAMEWORK = {
         'service_user': '800/minute',
         'registration_validation': '30/minute',
     },
-    'DEFAULT_AUTHENTICATION_CLASSES': (
+    'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'edx_rest_framework_extensions.auth.jwt.authentication.JwtAuthentication'
-    ),
+    ],
 }
 
 SWAGGER_SETTINGS = {
