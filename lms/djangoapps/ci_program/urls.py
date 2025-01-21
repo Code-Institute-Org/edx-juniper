d0b885ff3687d0c17363645e071ea9fc918dbc19
Sean Murphy <mail.seanmurphy@gmail.com>
2025-01-21 10:05:36 +0000

    Add sort by bookmark date feature

--- lms/djangoapps/ci_program/urls.py
+++ lms/djangoapps/ci_program/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from .views import show_programs, show_program_bookmarks
+from .views import show_programs, show_program_bookmarks, set_bookmark_preference
 
 urlpatterns = [
     url(r'^(?P<program_name>[\w\-]+)$', show_programs, name='show_programs'),
     url(r'^(?P<program_name>[\w\-]+)/bookmarks', show_program_bookmarks, name='show_program_bookmarks'),
+    url(r'^(?P<program_name>[\w\-]+)/bookmark_preference', set_bookmark_preference, name='set_bookmark_preference'),
 ]
